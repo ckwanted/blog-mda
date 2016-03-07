45536b65d0d892c4e65f0f12c723e82a7f4a8ee0
luckys <cfour@openmailbox.org>
2016-03-07 16:39:29 +0000

    Definiendo arquitectura

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog Personal</title>
+
+    @include('partials.styles')
+
+</head>
+<body>
+
+    <div class="container">
+        <h1>Blog MDA</h1>
+    </div>
+
+    @include('partials.scripts')
+</body>
+</html>
