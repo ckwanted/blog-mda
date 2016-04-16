1eb15d311f13492189be14eb64f6c3c9c21879bb
luckys <cfour@openmailbox.org>
2016-04-16 14:40:01 +0100

    Correcion de errores

--- config/app.php
+++ config/app.php
@@ -145,8 +145,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        /*Intervention\Image\ImageServiceProvider::class,*/
-
     ],
 
     /*
@@ -197,7 +195,6 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
-        //'Image'     => Intervention\Image\Facades\Image::class,
     ],
 
 ];
