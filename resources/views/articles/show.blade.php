2e0ea16904a433c7033744fd64e8d79b2710fee7
Yazaida Hernández <yazaida.ha@gmail.com>
2016-04-27 18:08:42 +0100

    Demo me gusta

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -29,6 +29,22 @@
         </div>
 
     </div>
+    <div class="row">
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.6";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+        <!-- Your like button code -->
+        <div class="col-md-offset-2">
+            <div class="fb-like" data-href="http://www.desarrolloweb.com/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+        </div>
+    </div>
     <hr>
     @include('comments.show')
 
